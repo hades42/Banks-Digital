e4bf432adb668809fbb935d4660b19e3cf5e7d32
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2020-04-30 14:16:08 +1000

    finish section description

--- src/index.js
+++ src/index.js
@@ -11,17 +11,56 @@ import "./scss/main.scss";
 const dropBtn = document.querySelector(".header__list-dropbtn");
 const dropList = document.querySelector(".header__drop-list");
 
+let currentSlider = 0;
+const slides = document.querySelectorAll(".brands__slider");
+const dots = document.querySelectorAll(".brands__dots-dot");
+const slidesArr = [...slides];
+const dotsArr = [...dots];
+const prev = document.getElementById("prev-btn");
+const next = document.getElementById("next-btn");
+
+// console.log(prev);
+// console.log(next);
 dropBtn.addEventListener("click", (e) => {
   if (dropList.classList.contains("actived")) {
     dropList.classList.remove("actived");
   } else {
     dropList.classList.add("actived");
   }
-
-  //   console.log(123);
 });
 
-// dropBtn.addEventListener("mouseleave", () => {
-//   //   console.log(456);
-//   dropList.classList.remove("actived");
-// });
+function updateCurrent(currentIndex) {
+  slides[currentIndex].className = "brands__slider active";
+  dots[currentIndex].className = "brands__dots-dot active";
+}
+
+// prev btn
+prev.addEventListener("click", () => {
+  slides[currentSlider].className = "brands__slider right";
+  dots[currentSlider].className = "brands__dots-dot";
+  currentSlider = currentSlider - 1;
+  if (currentSlider < 0) {
+    currentSlider = slides.length - 1;
+  }
+  updateCurrent(currentSlider);
+});
+// next btn
+next.addEventListener("click", () => {
+  slides[currentSlider].className = "brands__slider left";
+  dots[currentSlider].className = "brands__dots-dot";
+  currentSlider = currentSlider + 1;
+  if (currentSlider > slides.length - 1) {
+    currentSlider = 0;
+  }
+  updateCurrent(currentSlider);
+});
+
+dotsArr.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    dotsArr.forEach((el) => (el.className = "brands__dots-dot"));
+    slidesArr.forEach((el) => (el.className = "brands__slider"));
+    updateCurrent(index);
+  });
+});
+
+updateCurrent(currentSlider);
